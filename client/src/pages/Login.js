1768e1eb59ffe4f3ca5a71e20b2ee8c9b765e67d
Abhishek Kumar <abhikumar21@iitk.ac.in>
2023-06-10 11:13:40 +0530

    gitignore

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,9 +3,24 @@ import './Login.css'
 import Animage from '../images/p7.jpg'
 
 const Login = () => {
+   const [data, setData] = useState({
+       username:"",
+       email:"",
+       password:""
 
-    const [user, setUser] = useState(true)
+   })
+   const handleChange = (e) => {
+    setData({...data, [e.target.name]: e.target.value})
+   }
 
+   const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    
+   }
+
+
+    const [user, setUser] = useState(false)
     const changeUser = (e) => {
         if(user) setUser(false)
         else setUser(true)
@@ -32,10 +47,11 @@ const Login = () => {
             <div className="box">
             <h2 className='reg_head'>Register</h2>
             <form className="inputs">
-                <input type="text" placeholder='Username' />
-                <input type="text" placeholder='E-mail' />
-                <input type="text" placeholder='Password' />
-                <button className='login_btn'>Sign Up</button>
+                <input type="text" placeholder='Username' name="username" value={data.username} onChange={handleChange} /> 
+                <input type="text" placeholder='E-mail' name="email" value={data.email} onChange={handleChange} />
+                <input type="text" placeholder='Password' name="password" value={data.password} onChange={handleChange} />
+                                                                                          {/* onChange={(e)=>handleChange(e)} */}
+                <button className='login_btn' onClick={(e)=>handleSubmit(e)}>Sign Up</button>
             </form>
             <h4>Already have an account <a onClick={()=>changeUser()}>Login</a></h4>
           </div>
